1016c92d128bd2e9a4b8d6d6cd2a80b1df8ee2e3
Vaisakh K M <vaisakhkm2625@gmail.com>
2025-11-03 03:44:28 +0530

    main index.js
    use turso's libsql service to store k-v from cloudflare worker

--- src/index.js
+++ src/index.js
@@ -10,6 +10,58 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+
+		const url = new URL(request.url);
+		const params= url.searchParams;
+
+		let return_value = {id:"", count:""};
+
+		const id = params.get("id")
+
+		console.log(request.method);
+		console.log(params.get("id"));
+
+		if(!params.get("id")) return new Response("require an id as query param", {status: 400});
+
+
+		return_value = await fetch(env.TURSO_COUNT_DB_URL , {
+			method: "POST",
+			headers: {
+				//Authorization: `Bearer ${authToken}`,
+				Authorization: `Bearer ${env.TURSO_COUNT_DB_TOKEN}`,
+				"Content-Type": "application/json",
+			},
+
+			body: JSON.stringify({
+				requests: [
+					{ type: "execute", stmt: {
+						//sql: "insert into counters(id,value) values(?,1) on conflict(id) do update set value = value+1;",
+						sql: "insert into counters(id,value) values(?,1) on conflict(id) do update set value = value+1 RETURNING id,value;",
+						args: [{type: "text",value:id}]
+					} },
+					{ type: "close" },
+				],
+			}),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				//console.log(JSON.stringify(data))
+				//let rvalue = { id: data?.results[0]?.result?.rows[0]?.value, count: data?.results[0]?.result?.rows[1]?.value}
+				let rvalue = { id: data.results[0].response.result.rows[0][0].value, count: data.results[0].response.result.rows[0][1].value}
+				return rvalue;
+
+			})
+			.catch((err) => console.log(err));
+
+		console.log(JSON.stringify(return_value));
+
+
+		return new Response(JSON.stringify(return_value),{
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",      // <- allow requests from any site
+        "Access-Control-Allow-Methods": "GET",   // optional
+      }
+    });
 	},
 };
